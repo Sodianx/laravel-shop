5fd6c780c5570fb03c4daaf1efd6d5ee3bbded8a
Sodian <188007086@qq.com>
2018-08-04 07:44:29 +0000

    创建订单

--- routes/web.php
+++ routes/web.php
@@ -40,7 +40,8 @@ Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy'
 
          Route::post('cart', 'CartController@add')->name('cart.add');
          Route::get('cart', 'CartController@index')->name('cart.index');
-          Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
+         Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
+         Route::post('orders', 'OrdersController@store')->name('orders.store');
     });
 
  
