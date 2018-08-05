3ea191af0c5ca3ca1e75d2b48a35d06ce9bdd744
Sodian <188007086@qq.com>
2018-08-05 06:48:51 +0000

    用户界面 - 申请退款

--- routes/web.php
+++ routes/web.php
@@ -55,6 +55,8 @@ Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy'
 
           Route::get('orders/{order}/review', 'OrdersController@review')->name('orders.review.show');
         Route::post('orders/{order}/review', 'OrdersController@sendReview')->name('orders.review.store');
+
+         Route::post('orders/{order}/apply_refund', 'OrdersController@applyRefund')->name('orders.apply_refund');
     });
 
  
