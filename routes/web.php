fa74b4704ed666bf4db188f1e98c3d4a42281948
Sodian <188007086@qq.com>
2018-08-05 08:20:38 +0000

    用户界面 - 检查优惠券

--- routes/web.php
+++ routes/web.php
@@ -57,6 +57,8 @@ Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy'
         Route::post('orders/{order}/review', 'OrdersController@sendReview')->name('orders.review.store');
 
          Route::post('orders/{order}/apply_refund', 'OrdersController@applyRefund')->name('orders.apply_refund');
+
+         Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
     });
 
  
